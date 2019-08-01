63782db43f694389a91da699bec64b045557d1ab
Rihards Zauls <rihards.zauls@gmail.com>
2019-08-01 20:55:15 +0300

    added file type checking

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use std::process;
 use structopt::StructOpt;
 
 /**
- * TODO: add type checking (append / and change color for directories)
+ * TODO: print dirs first, then files
+ * TODO: convert bytes into more readable format (mb, gb, etc...)
  */
 #[derive(StructOpt, Debug)]
 struct Opt {
@@ -28,19 +29,29 @@ fn run(dir: &PathBuf) -> Result<(), Box<Error>> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
-            let file_name = entry
+            let path = entry.path();
+            let mut file_name_colored: colored::ColoredString;
+            let mut file_name = entry
                 .file_name()
                 .into_string()
                 .or_else(|f| Err(format!("Invalid entry: {:?}", f)))?;
+            // check if file is dir
+            if path.is_dir() {
+                file_name += "/";
+                file_name_colored = file_name.green();
+            } else {
+                file_name_colored = file_name.yellow();
+            }
             let metadata = entry.metadata()?;
             let size = metadata.len().to_string() + " B";
             let modified: DateTime<Local> = DateTime::from(metadata.modified()?);
 
             println!(
-                "{:>10} {} {}",
+                // left padding 15 chars to fit large filesizes
+                "{:>15} {} {}",
                 size.bright_blue(),
-                modified.format("%_d %b %H:%M").to_string().green(),
-                file_name.bright_yellow()
+                modified.format("%_d %b %H:%M").to_string(),
+                file_name_colored
             );
         }
     }
