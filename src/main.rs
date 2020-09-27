bec818b2c1019b9c81423b96ff4dc7e91860129b
Rihards Zauls <rihards.zauls@gmail.com>
2020-09-27 14:40:48 +0300

    print dirs first, then files, remove owner flag, add year

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,12 @@ use structopt::StructOpt;
 /**
  * Simple ls clone with windows-friendly coloured formatting
  *
- * TODO: print dirs first, then files
+ * TODO: reformat the obvious code duplication away
+ * TODO: fix alias/symlinm issues (~/ etc)
  * TODO: figure out line wrapping (cut filenames after x chars?)
- * TODO: fix ownership flag
  */
 #[derive(StructOpt, Debug)]
 struct Options {
-    /// Show ownership details
-    #[structopt(short = "o", long = "owner")]
-    owner: bool,
     /// Show all files (including dotfiles)
     #[structopt(short = "a", long = "all")]
     all: bool,
@@ -28,58 +25,126 @@ struct Options {
 
 fn main() {
     let opt = Options::from_args();
-    if let Err(ref e) = run(&opt.path, &opt.all, &opt.owner) {
+    if let Err(ref e) = run(&opt.path, &opt.all) {
         println!("{}", e);
         process::exit(1);
     }
 }
 
-fn run(dir: &PathBuf, all: &bool, owner: &bool) -> Result<(), Box<dyn Error>> {
+fn _type_of<T>(_: T) -> &'static str {
+    std::any::type_name::<T>()
+}
+
+fn print_dir(input: std::fs::DirEntry, all: &bool) {
+    let file_name_colored: colored::ColoredString;
+    let file_name = input.file_name().into_string();
+    // check if file is dotfile
+    match file_name {
+        Ok(mut item) => {
+            if (all == &false) && (item.starts_with(".")) {
+                return;
+            }
+
+            item += "/";
+            file_name_colored = item.green();
+
+            match input.metadata() {
+                Ok(metadata) => {
+                    // format size
+                    let size: String;
+                    match metadata.len() {
+                        0 => size = "...".to_string(),
+                        1..=1024 => size = bytefmt::format_to(metadata.len(), bytefmt::Unit::B),
+                        1025..=1048567 => {
+                            size = bytefmt::format_to(metadata.len(), bytefmt::Unit::KB)
+                        }
+                        1048576..=1073741824 => {
+                            size = bytefmt::format_to(metadata.len(), bytefmt::Unit::MB)
+                        }
+                        _ => size = bytefmt::format_to(metadata.len(), bytefmt::Unit::GB),
+                    };
+                    let modified: DateTime<Local> = match metadata.modified() {
+                        Ok(time) => DateTime::from(time),
+                        Err(e) => panic!("PANIC: {:?}", e),
+                    };
+
+                    println!(
+                        // left padding 15 chars to fit large filesizes
+                        "{:>15} {} {}",
+                        size.bright_blue(),
+                        modified.format("%_d %b %Y %H:%M").to_string(),
+                        file_name_colored
+                    );
+                }
+                Err(e) => panic!("PANIC: {:?}", e),
+            }
+        }
+
+        Err(e) => panic!("PANIC: {:?}", e),
+    }
+}
+
+fn print_item(input: std::fs::DirEntry, all: &bool) {
+    let file_name_colored: colored::ColoredString;
+    let file_name = input.file_name().into_string();
+    // check if file is dotfile
+    match file_name {
+        Ok(item) => {
+            if (all == &false) && (item.starts_with(".")) {
+                return;
+            }
+
+            file_name_colored = item.yellow();
+
+            match input.metadata() {
+                Ok(metadata) => {
+                    // format size
+                    let size: String;
+                    match metadata.len() {
+                        0 => size = "...".to_string(),
+                        1..=1024 => size = bytefmt::format_to(metadata.len(), bytefmt::Unit::B),
+                        1025..=1048567 => {
+                            size = bytefmt::format_to(metadata.len(), bytefmt::Unit::KB)
+                        }
+                        1048576..=1073741824 => {
+                            size = bytefmt::format_to(metadata.len(), bytefmt::Unit::MB)
+                        }
+                        _ => size = bytefmt::format_to(metadata.len(), bytefmt::Unit::GB),
+                    };
+                    let modified: DateTime<Local> = match metadata.modified() {
+                        Ok(time) => DateTime::from(time),
+                        Err(e) => panic!("PANIC: {:?}", e),
+                    };
+
+                    println!(
+                        // left padding 15 chars to fit large filesizes
+                        "{:>15} {} {}",
+                        size.bright_blue(),
+                        modified.format("%_d %b %Y %H:%M").to_string(),
+                        file_name_colored
+                    );
+                }
+                Err(e) => panic!("PANIC: {:?}", e),
+            }
+        }
+
+        Err(e) => panic!("PANIC: {:?}", e),
+    }
+}
+
+fn run(dir: &PathBuf, all: &bool) -> Result<(), Box<dyn Error>> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            let file_name_colored: colored::ColoredString;
-            let mut file_name = entry
-                .file_name()
-                .into_string()
-                .or_else(|f| Err(format!("Invalid entry: {:?}", f)))?;
-            // check if file is dotfile
-            if (all == &false) && (file_name.starts_with(".")) {
-                continue;
+            let item = entry?;
+            if item.path().is_dir() {
+                print_dir(item, all);
             }
-            if owner == &true {
-                println!("ownership not implemented")
+        }
+        for entry in fs::read_dir(dir)? {
+            let item = entry?;
+            if !item.path().is_dir() {
+                print_item(item, all);
             }
-            // check if file is dir
-            if path.is_dir() {
-                file_name += "/";
-                file_name_colored = file_name.green();
-            } else {
-                file_name_colored = file_name.yellow();
-            }
-            let metadata = entry.metadata()?;
-
-            // format size
-            let size: String;
-            match metadata.len() {
-                0 => size = "...".to_string(),
-                1..=1024 => size = bytefmt::format_to(metadata.len(), bytefmt::Unit::B),
-                1025..=1048567 => size = bytefmt::format_to(metadata.len(), bytefmt::Unit::KB),
-                1048576..=1073741824 => {
-                    size = bytefmt::format_to(metadata.len(), bytefmt::Unit::MB)
-                }
-                _ => size = bytefmt::format_to(metadata.len(), bytefmt::Unit::GB),
-            };
-            let modified: DateTime<Local> = DateTime::from(metadata.modified()?);
-
-            println!(
-                // left padding 15 chars to fit large filesizes
-                "{:>15} {} {}",
-                size.bright_blue(),
-                modified.format("%_d %b %H:%M").to_string(),
-                file_name_colored
-            );
         }
     }
     Ok(())
